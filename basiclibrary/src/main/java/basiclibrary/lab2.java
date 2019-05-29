3326dcce6ecd2a1d23690ca1346bb4b2575eab0a
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-05-29 13:54:31 -0700

    compleated lab2 with all test

--- basiclibrary/src/main/java/basiclibrary/lab2.java
+++ basiclibrary/src/main/java/basiclibrary/lab2.java
@@ -25,6 +25,29 @@ public class lab2 {
       return false;
     }
 
-//    public double calculatingAverages(){
-//    }
+    public double averages(int[] arr){
+        double sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        double avg = sum / (double) arr.length;
+        return avg;
+    }
+
+    public double avgAverages(int[][] arr){
+        double lowestAvg = 0;
+        double[] output = new double[arr.length];
+        for(int i = 0; i < output.length; i++){
+            if(i == 0){
+                output[i] = averages(arr[i]);
+                lowestAvg = output[i];
+            } else {
+                output[i] = averages(arr[i]);
+                if(lowestAvg > output[i]){
+                    lowestAvg = output[i];
+                }
+            }
+        }
+        return lowestAvg;
+    }
 }
