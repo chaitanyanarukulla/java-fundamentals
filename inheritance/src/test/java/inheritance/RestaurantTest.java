acd9f387210a3b21a5759fcc06025f592a684bfe
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-06-04 12:31:41 -0700

    compleated step 1

---
+++ inheritance/src/test/java/inheritance/RestaurantTest.java
@@ -0,0 +1,24 @@
+package inheritance;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RestaurantTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getStars() {
+    }
+
+    @Test
+    public void getpriceCategory() {
+    }
+
+    @Test
+    public void toString1() {
+    }
+}
