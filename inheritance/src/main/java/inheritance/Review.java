2bcfd6fcc4079cbf42789f9fc7ff32755a52a588
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-06-05 14:58:20 -0700

    updated readme

--- inheritance/src/main/java/inheritance/Review.java
+++ inheritance/src/main/java/inheritance/Review.java
@@ -5,6 +5,8 @@ public class Review {
     private String author;
     private Integer stars;
     private Restaurant restaurant;
+    private Shop shop;
+    private Theater theater;
 
     public Review(String body, int stars, String author) {
         this.body = body;
@@ -19,6 +21,23 @@ public class Review {
         this.restaurant = restaurant;
         restaurant.addReview(this);
         restaurant.update_Stars();
+    }
+
+    public Review(String body, Integer stars, String author,Shop shop) {
+        this.body = body;
+        this.stars = stars;
+        this.author = author;
+        this.shop = shop;
+//        Shop.addReview(this);
+//        Shop.update_Stars();
+    }
+    public Review(String body, Integer stars, String author,Theater theater) {
+        this.body = body;
+        this.stars = stars;
+        this.author = author;
+        this.theater = theater;
+//        theater.addReview(this);
+//        theater.update_Stars();
     }
 
     public Integer getStars() {
