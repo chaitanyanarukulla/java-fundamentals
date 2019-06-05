1dd18a7b1b37cc506c6b1a3de64caabc749f8dcd
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-06-05 10:45:33 -0700

    updated review and Reaturant class

--- inheritance/src/main/java/inheritance/Review.java
+++ inheritance/src/main/java/inheritance/Review.java
@@ -1,12 +1,49 @@
 package inheritance;
 
 public class Review {
-    String body;
-    String author;
-    float stars;
+    private String body;
+    private String author;
+    private Integer stars;
     private Restaurant restaurant;
 
-    public Review(String body, String author, int stars){
-        return;
+    public Review(String body, int stars, String author) {
+        this.body = body;
+        this.stars = stars;
+        this.author = author;
+    }
+
+    public Review(String body, Integer stars, String author,Restaurant restaurant) {
+        this.body = body;
+        this.stars = stars;
+        this.author = author;
+        this.restaurant = restaurant;
+        restaurant.addReview(this);
+        restaurant.update_Stars();
+    }
+
+    public Integer getStars() {
+        return stars;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Restaurant: %s\nReview: %s \nNumber of Stars: %s\nAuthor: %s", restaurant.getName(), body, stars, author);
     }
 }
--- inheritance/src/test/java/inheritance/RestaurantTest.java
+++ inheritance/src/test/java/inheritance/RestaurantTest.java
@@ -8,32 +8,35 @@ public class RestaurantTest {
 
     @Test
     public void test_Restaurant_constructor() {
-        Restaurant classUnderTest = new Restaurant("chai", 1.2, "$$");
+        Restaurant classUnderTest = new Restaurant("chai", "$");
         assertEquals("should return name chai","chai",classUnderTest.getName());
     }
 
     @Test
     public void test_Restaurant_constructor_getName() {
-        Restaurant classUnderTest = new Restaurant("chai", 1.2, "$$");
+        Restaurant classUnderTest = new Restaurant("chai", "$$");
         assertEquals("should return name chai","chai",classUnderTest.getName());
     }
 
     @Test
     public void test_Restaurant_constructor_getPrice() {
-        Restaurant classUnderTest = new Restaurant("chai", 1.2, "$$");
+        Restaurant classUnderTest = new Restaurant("chai", "$$");
         assertEquals("should return $$","$$", classUnderTest.getpriceCategory());
     }
 
     @Test
-    public void test_getName() {
+    public void test_testToString() {
+        Restaurant classUnderTest = new Restaurant("Will", "$$");
+        String expectedOutput = "Restaurant Name: Will\nPrice: $$ \nNumber of Stars: null";
+        assertEquals(expectedOutput, classUnderTest.toString());
     }
-
     @Test
-    public void test_getStars() {
+    public void test_AddReview() {
+        Restaurant classUnderTest = new Restaurant("Luke", "$$");
+        Review restaurantReview = new Review("Best place for PBR", 5, "Luke.ll");
+        classUnderTest.addReview(restaurantReview);
+        assertTrue("Should contain review", classUnderTest.containsReview(restaurantReview));
     }
 
 
-    @Test
-    public void test_toString1() {
-    }
 }
